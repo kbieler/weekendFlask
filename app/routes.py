097e98db1325b5490920444e5ce48e6f6fc5dee9
kbieler <kristenbieler@gmail.com>
2022-05-22 19:03:26 -0500

    more pages

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app
 from flask import render_template
 import requests as r
-from .templates.services import getEpisode, getImages
+from .templates.services import getEpisode, getImages, getChar
 
 
 @app.route('/')
@@ -17,8 +17,22 @@ def ricks():
 @app.route('/about')
 def about():
     contexts = getImages()
-    return render_template('about.html', **contexts)
+    return render_template('about.html', **contexts) 
 
 @app.route('/schwifty')
 def schwifty():
     return render_template('schwifty.html')
+
+@app.route('/squanch.html')
+def squanch():
+    context = getChar()
+    return render_template('squanch.html', **context)
+
+@app.route('/go.html')
+def go():
+    return render_template('go.html')
+
+@app.route('/yourself.html')
+def yourself():
+    context = getEpisode()
+    return render_template('yourself.html', **context)
--- app/templates/services.py
+++ app/templates/services.py
@@ -31,8 +31,10 @@ class Character:
     def __init__(self, imgdict):
         self.name = imgdict['name']
         self.image = imgdict['image']
-    
-    
+        self.id = imgdict['id']
+        self.status = imgdict['status']
+        self.species = imgdict['species']
+
 
 def getImages():
     data = r.get('https://rickandmortyapi.com/api/character')
@@ -41,15 +43,34 @@ def getImages():
     else:
         return 'broken api'
 
-    api_imgs = data['results']
+    api_char = data['results']
     avatar_imgs= {}
-    for char in api_imgs:
-        ea_img = Character(char)
-        avatar_imgs[ea_img.name] = ea_img.image
+    for char in api_char:
+        ea_char = Character(char)
+        avatar_imgs[ea_char.name] = ea_char.image
     return {
-        'characters': random.choice(list(avatar_imgs.values()))
+        'characters': random.choice(list(avatar_imgs.values())),
+        'avatars' : random.choice(list(avatar_imgs.values())),
+        'another' : random.choice(list(avatar_imgs.values())),  
+        'morepics': random.choice(list(avatar_imgs.values())),
+        'onemore' : random.choice(list(avatar_imgs.values()))
+        }
+
+def getChar():
+    data = r.get('https://rickandmortyapi.com/api/character')
+    if data.status_code == 200:
+        data = data.json()
+    else:
+        return 'broken api'
+
+    api_char = data['results']
+    rm_char= {}
+    for char in api_char[:75]:
+        ea_char = Character(char)
+        rm_char[ea_char.id] = ea_char
+    return {
+        'all_char': rm_char
     }
 
 
-   
-    
+
